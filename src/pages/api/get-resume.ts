2c41f56e1c445aa3ce16843c32b3f3702883ca24
zeyadEsam <ziadesamwd@gmail.com>
2023-05-12 05:31:43 +0300

    refactoring the get-resume api endpoint to stream the file to the response

--- src/pages/api/get-resume.ts
+++ src/pages/api/get-resume.ts
@@ -1,16 +1,42 @@
 import fs from "fs";
 import path from "path";
 
-export default async function (req: any, res: any) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
   const filePath = path.join(process.cwd(), "public", "zeyad-essam-resume.pdf");
+  const stat = fs.statSync(filePath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
 
-  const fileContent = fs.readFileSync(filePath);
+  if (range) {
+    // Parse the range header to get the start and end bytes of the requested chunk
+    const [startStr, endStr] = range.replace(/bytes=/, "").split("-");
+    const start: number = parseInt(startStr, 10);
+    const end: number = parseInt(endStr, 10);
+    const chunkSize = end - start + 1;
+    const fileStream = fs.createReadStream(filePath, {
+      start: start,
+      end: end,
+    });
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunkSize,
+      "Content-Type": "application/pdf",
+      "Content-Disposition": "attachment; zeyad-essam-resume.pdf",
+    };
 
-  res.setHeader("Content-Type", "application/pdf");
-  res.setHeader(
-    "Content-Disposition",
-    "attachment; filename=zeyad-essam-resume.pdf"
-  );
+    res.writeHead(206, headers);
+    fileStream.pipe(res);
+  } else {
+    const headers = {
+      "Content-Length": fileSize,
+      "Content-Type": "application/pdf",
+      "Content-Disposition": "attachment; zeyad-essam-resume.pdf",
+    };
 
-  res.send(fileContent);
+    res.writeHead(200, headers);
+    fs.createReadStream(filePath).pipe(res);
+  }
 }
