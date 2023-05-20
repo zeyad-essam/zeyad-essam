7d86dc6fae6842ba90015308f5db9413612b60ff
zeyadEsam <ziadesamwd@gmail.com>
2023-05-20 06:00:27 +0300

    adding some adjustments to the footer section

--- src/components/Layout/Footer/Contacts/index.tsx
+++ src/components/Layout/Footer/Contacts/index.tsx
@@ -6,7 +6,6 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
-
 import UpworkIcon from "../../../../../public/ui/upwork.svg";
 
 import styles from "./Contacts.module.css";
@@ -15,7 +14,7 @@ const Contacts = () => {
   return (
     <div className={styles.contacts}>
       <div className={styles.phone}>
-        <a href="tel:+01143210887">
+        <a href="tel:+201143210887">
           <i>
             <PhoneIcon />
           </i>
@@ -38,7 +37,10 @@ const Contacts = () => {
             </a>
           </li>
           <li>
-            <a target="_blank" href="mailto:zeyadessam162@gmail.com">
+            <a
+              target="_blank"
+              href="https://www.upwork.com/freelancers/~016c139fa3effee5af"
+            >
               <UpworkIcon />
             </a>
           </li>
